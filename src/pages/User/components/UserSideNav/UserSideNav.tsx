177584215a92724a72fa1b4cba9475204bba9ce2
Thanh1911 <phantomcat191@gmail.com>
2025-11-19 16:02:20 +0700

    added SideNav

--- src/pages/User/components/UserSideNav/UserSideNav.tsx
+++ src/pages/User/components/UserSideNav/UserSideNav.tsx
@@ -1,9 +1,62 @@
+import {Link} from "react-router-dom";
+import paths from "../../../../constants/paths.ts";
 
 const UserSideNav = () => {
   return (
     <div>
-      NavSideBar
+      <div className='flex items-center border-b border-b-gray-200 py-4'>
+        <Link to={paths.profile} className='h-12 w-12 flex-shrink-0 overflow-hidden rounded-full border-black/10'>
+          <img src='https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn' alt='anh'
+               className='h-full w-full object-cover'/>
+        </Link>
+        <div className="flex-grow pl-4">
+          <div className="mb-1 truncate font-semibold text-gray-600">
+            pthanh
+          </div>
+          <Link to={paths.profile} className='flex items-center capitalize text-gray-500'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" className="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"/>
+            </svg>
+            Sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+      <div className="mt-7">
+        <Link to={paths.profile} className='flex mt-4 items-center capitalize text-orange transition-colors'>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" className="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"/>
+            </svg>
+          </div>
+          Tài khoản của tôi
+        </Link>
+        <Link to={paths.changePassword} className='flex mt-4 items-center capitalize text-gray-600 transition-colors'>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" className="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"/>
+            </svg>
+          </div>
+          Đổi mật khẩu
+        </Link>
+        <Link to={paths.historyPurchase} className='flex mt-4 items-center capitalize text-gray-600 transition-colors'>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" className="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"/>
+            </svg>
+          </div>
+          Đơn mua
+        </Link>
+      </div>
     </div>
+
   );
 };
 
